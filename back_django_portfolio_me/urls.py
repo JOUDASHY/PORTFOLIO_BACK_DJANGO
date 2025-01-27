8e8ed5aadab3662c46c4415914c9d46c2ced3b92
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-01-27 08:43:28 +0300

    fix urls

--- back_django_portfolio_me/urls.py
+++ back_django_portfolio_me/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.http import HttpResponse
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('', lambda request: HttpResponse("Welcome to the Portfolio API!")), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
