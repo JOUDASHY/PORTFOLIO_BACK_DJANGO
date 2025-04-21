b986fda3e7c3c0cd2b251ee584ae4b68d8267086
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-04-21 21:50:31 +0300

    add req

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,7 @@
-# Fichier optimisé pour Hostinger
+# Fichier /var/www/PORTFOLIO_BACK_DJANGO/gunicorn_conf.py
 bind = "0.0.0.0:8000"
-workers = 2
-timeout = 120
-keepalive = 75
-max_requests = 500
-max_requests_jitter = 50
+workers = 1              # Réduisez à 1 worker pour debug
+timeout = 300            # Augmentez à 5 min
+keepalive = 120          # Réduisez keepalive
+max_requests = 0         # Désactivez le recyclage pour tests
+max_requests_jitter = 0  # Désactivez le jitter
