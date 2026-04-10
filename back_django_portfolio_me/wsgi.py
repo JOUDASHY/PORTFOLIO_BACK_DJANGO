bd531307849d90c510b516086303b645f2d8f23f
JOUDASHY <alitsiryeddynilsen@gmail.com>
2026-04-10 15:01:29 +0300

    a

--- back_django_portfolio_me/wsgi.py
+++ back_django_portfolio_me/wsgi.py
@@ -8,10 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_django_portfolio_me.settings')
 
-application = get_wsgi_application()
-app=application
+# Guard against wfastcgi double-init (populate() isn't reentrant)
+if not django.apps.apps.ready:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+else:
+    from django.core.handlers.wsgi import WSGIHandler
+    application = WSGIHandler()
